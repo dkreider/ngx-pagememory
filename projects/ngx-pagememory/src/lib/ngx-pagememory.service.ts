dcdb81a555329d2848581bac09d0f937de5a4c57
Daniel Kreider <rxgmt61of@mozmail.com>
2022-03-29 20:41:03 -0300

    Adding the functions to set and retrieve last page, as well as the router subscription

--- projects/ngx-pagememory/src/lib/ngx-pagememory.service.ts
+++ projects/ngx-pagememory/src/lib/ngx-pagememory.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Router, RouterEvent } from '@angular/router';  
 
 @Injectable({
   providedIn: 'root'
 })
 export class NgxPagememoryService {
 
-  constructor() { }
+  lastRouteKey = "lastRoute";
+  _lastRoute = localStorage.getItem(this.lastRouteKey);
+
+  constructor(private router: Router) { }
+
+  register(): void {
+    let lastRoute = this.getLastRoute();
+    if (lastRoute != null) {
+      this.router.navigateByUrl(lastRoute);
+    }
+
+    this.router.events.subscribe((event: RouterEvent) => {
+      if (event.url)
+        this.setLastRoute(event.url);
+    });
+  }
+
+  public setLastRoute(value: string) {
+    localStorage.setItem(this.lastRouteKey, value);
+  }
+
+  public getLastRoute(): string | null {
+    return localStorage.getItem(this.lastRouteKey);
+  }
+
 }
