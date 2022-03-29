dee7ef09deb78eeafb23838d06ba6cf68cde4b60
Daniel Kreider <rxgmt61of@mozmail.com>
2022-03-29 20:41:30 -0300

    Add some basic testing

--- projects/ngx-pagememory/src/lib/ngx-pagememory.service.spec.ts
+++ projects/ngx-pagememory/src/lib/ngx-pagememory.service.spec.ts
@@ -1,16 +1,32 @@
 import { TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
 
 import { NgxPagememoryService } from './ngx-pagememory.service';
 
 describe('NgxPagememoryService', () => {
   let service: NgxPagememoryService;
+  const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: Router, useValue: routerSpy
+        }
+      ]
+    });
     service = TestBed.inject(NgxPagememoryService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should set and return last route', () => {
+    let fakeRoute = "/my/path/inside/angular/app";
+    service.setLastRoute(fakeRoute);
+    
+    let result = service.getLastRoute();
+    expect(result).toEqual(fakeRoute);
+  })
 });
