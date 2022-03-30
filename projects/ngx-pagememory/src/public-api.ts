ff6cc0039ec2bb7a842698eca4d5efda7acc82f0
Daniel Kreider <rxgmt61of@mozmail.com>
2022-03-30 17:15:00 -0300

    Only export the module

--- projects/ngx-pagememory/src/public-api.ts
+++ projects/ngx-pagememory/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of ngx-pagememory
  */
 
-export * from './lib/ngx-pagememory.service';
-export * from './lib/ngx-pagememory.component';
 export * from './lib/ngx-pagememory.module';
